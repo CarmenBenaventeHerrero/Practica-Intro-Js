43cbcd98cc25569da398f5d044b2e04839dd2362
Eduardo Aguilar <eduardoaguilartirado@gmail.com>
2022-11-16 11:56:07 +0100

    switch exercise

---
+++ switch.js
@@ -0,0 +1,24 @@
+const note = 6;
+
+switch(note) {
+  case 0:
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+    console.log('suspenso')
+  case 5:
+    console.log('suficiente')
+  case 6:
+    console.log('bien')
+  case 7:
+  case 8:
+    console.log('notable')
+  case 9:
+    console.log('sobresaliente')
+  case 10:
+    console.log('mdh')
+  default:
+    console.log('nota no valida')
+}
+
